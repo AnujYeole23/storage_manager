5fede6beeb67041ff8c909012539ed3c64b98039
anujyeole23 <anuj.yeole@centralogic.net>
2024-11-25 17:15:59 +0530

    Local_file_implementation: web

--- lib/core/web_local_file.dart
+++ lib/core/web_local_file.dart
@@ -1,30 +1,58 @@
-import 'dart:io';
 import 'dart:html';
 
 import 'package:storage_manager/core/local_file.dart';
+import 'package:universal_io/io.dart';
 
 class WebLocalFile implements LocalFile {
+  static final CacheStorage? _cacheStorage = window.caches;
+
   @override
-  Future<bool> fileExists(String localPath) {
-    // TODO: implement fileExists
-    throw UnimplementedError();
+  Future<bool> fileExists(String localPath) async {
+    try {
+      if (_cacheStorage == null) {
+        return false; // CacheStorage is not supported
+      }
+
+      final cache =
+          await _cacheStorage!.open('web-local-cache'); // Cast to Cache
+      final response = await cache
+          .match(Uri.parse(localPath)); // Use match to find the resource
+
+      return response != null; // Check if resource exists
+    } catch (e) {
+      print('Error in fileExists: $e');
+      return false; // Handle errors gracefully
+    }
+  }
+
+  @override
+  Future<DateTime?> lastModified(String localPath) async {
+    try {
+      final metadataStorage = window.localStorage;
+      final metadata = metadataStorage['$localPath:lastModified'];
+
+      if (metadata != null) {
+        return DateTime.parse(metadata);
+      }
+    } catch (e) {
+      print('Error in lastModified: $e');
+    }
+    return null;
+  }
+
+  @override
+  Future<String> getPath(
+      {required String storagePath, Directory? cacheDir}) async {
+    try {
+      return Uri.parse(storagePath).toString();
+    } catch (e) {
+      print('Error in getPath: $e');
+      return '';
+    }
   }
 
   @override
   Future<Directory> getDownloadDirectory(Directory cacheDir) {
-    // TODO: implement getDownloadDirectory
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<String> getPath({required String storagePath, Directory? cacheDir}) {
-    // TODO: implement getPath
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<DateTime?> lastModified(String localPath) {
-    // TODO: implement lastModified
-    throw UnimplementedError();
+    throw UnimplementedError('getDownloadDirectory is not applicable for web.');
   }
 }
